c31ed78f96c6970541bfb841b2843bb1e3c75e8d
unknown <jokester8998@gmail.com>
2021-03-16 17:56:12 +0800

    make it better

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,5 @@
-insert into employee (EMPID, NAME, SALARY, EMAIL, JOBNAME) values ('001', 'Nicole','50000','nc@abc.com','Manager');
+insert into ROLES (NAME) values ('ADMIN');
+insert into ROLES (NAME) values ('USER');
 
-insert into employee (EMPID, NAME, SALARY, EMAIL, JOBNAME) values ('002', 'Desmond','200000','da@abc.com','Chairman');
+insert into USERS (username, enabled, password) values ('admin', 1, '$2y$12$LbNWLhG3lTqm4skk6VvZ8.9HYSy.SuPJoY6vudpQw49UYcd2XINky');
 
-insert into employee (EMPID, NAME, SALARY, EMAIL, JOBNAME) values ('003', 'Sebastian','45000','st@abc.com','Director');
-
-insert into employee (EMPID, NAME, SALARY, EMAIL, JOBNAME) values ('004', 'Jasmine','60000','ja@abc.com','Executive Director');
