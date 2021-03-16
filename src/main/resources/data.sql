8fdeea36813073e5aaf6a07c3ffa50e0e5e6c7d8
unknown <jokester8998@gmail.com>
2021-03-16 21:53:37 +0800

    make it better

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into ROLES (NAME) values ('ADMIN');
 insert into ROLES (NAME) values ('USER');
 
-insert into USERS (username, enabled, password) values ('admin', 1, '$2y$12$LbNWLhG3lTqm4skk6VvZ8.9HYSy.SuPJoY6vudpQw49UYcd2XINky');
+insert into USERS (user_id, username, enabled, password) values (1, 'admin', 1, '$2y$12$LbNWLhG3lTqm4skk6VvZ8.9HYSy.SuPJoY6vudpQw49UYcd2XINky');
 
