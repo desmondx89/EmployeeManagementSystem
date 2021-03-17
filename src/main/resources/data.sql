5500df10e59b2b59fa5ec66d163cf17d4e0a70be
unknown <jokester8998@gmail.com>
2021-03-17 09:50:09 +0800

    make it better

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,4 @@
-insert into ROLES (NAME) values ('ADMIN');
-insert into ROLES (NAME) values ('USER');
-
-insert into USERS (user_id, username, enabled, password) values (1, 'admin', true, '$2y$12$LbNWLhG3lTqm4skk6VvZ8.9HYSy.SuPJoY6vudpQw49UYcd2XINky');
+delete from roles where role_id > 2;
 
 insert into user_roles (role_id, user_id) values (1,1);
 insert into user_roles (role_id, user_id) values (2,1);
