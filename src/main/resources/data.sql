1a585bcfb04a12811a071d0b31b77f551d951885
unknown <jokester8998@gmail.com>
2021-03-17 09:58:36 +0800

    make it better

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,1 @@
-delete from roles where role_id > 2;
-
-insert into user_roles (role_id, user_id) values (1,1);
-insert into user_roles (role_id, user_id) values (2,1);
+delete from user_roles where role_id > 1;
